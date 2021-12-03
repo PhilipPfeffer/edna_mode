6fc338fb29641653ae49547a990d3ef3d8cb4225
Philip Mateo Pfeffer <philipmateopfeffer@Philip-Mateo-Pfeffer.local>
2021-12-03 16:45:43 +0000

    Thresholds added to arduino prediction code:

--- edna_mode/mean_embeddings.h
+++ edna_mode/mean_embeddings.h
@@ -2,5 +2,7 @@
 
 #define num_labels 3
 #define embedding_size 20
+extern const char *unknown_label;
 extern const char *mean_embeddings_labels[num_labels];
+extern const float thresholds[num_labels];
 extern const float mean_embeddings[num_labels][embedding_size];
